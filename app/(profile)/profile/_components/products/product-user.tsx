94568cbeef1ab53ee8a3dbff96532cb06132c44d
Sieryo <rioyudanto223@gmail.com>
2023-11-27 20:06:17 +0700

    bug fixes on product

--- app/(profile)/profile/_components/products/product-user.tsx
+++ app/(profile)/profile/_components/products/product-user.tsx
@@ -12,7 +12,7 @@ export const ProductUser = ({ item }: { item: TypeProduct }) => {
       <div>
         <div className=" lg:min-w-[220px] lg:min-h-[300px] bg-slate-200 "></div>
         <div className=" pt-3">
-          <h3 className=" font-bold text-lg">{item.name}</h3>
+          <h3 className=" font-bold text-lg max-w-[220px]">{item.name}</h3>
           <span className=" mt-1 block text-green-600">{formattedPrice}</span>
           <StatusPublish isPublished={item.isPublished} />
         </div>
